d087c621b42dab6207ec2ba9e0faf71bb4914753
Rafreaky Weyland <darklighterk25@gmail.com>
2020-05-02 01:46:33 -0500

    Implemented cogs
    Moved test command to Test Cog.

--- bot.py
+++ bot.py
@@ -4,14 +4,18 @@ import os
 import discord
 from discord.ext import commands
 
+# Cogs.
+from cogs.test import Test
+
 # Environment variables.
 from dotenv import load_dotenv
 load_dotenv()
-CHANNEL = int(os.getenv('DISCORD_CHANNEL')) # ⚠ Must be an integer.
+CHANNEL = int(os.getenv('DISCORD_CHANNEL'))  # ⚠ Must be an integer.
 GUILD = os.getenv('DISCORD_GUILD')
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
+
 
 @bot.event
 async def on_ready():
@@ -19,20 +23,18 @@ async def on_ready():
     for guild in bot.guilds:
         if guild.name == GUILD:
             break
-    print(f'\033[94m{bot.user} successfully connected to {guild.name}!\033[0m 😎')
-    activity = discord.Activity(name='the world end ☣', type=discord.ActivityType.watching)
+    print(
+        f'\033[94m{bot.user} successfully connected to {guild.name}!\033[0m 😎')
+    activity = discord.Activity(
+        name='the world end ☣', type=discord.ActivityType.watching)
     await bot.change_presence(activity=activity)
     channel = bot.get_channel(CHANNEL)
+    print('\033[94mTrying to send message to channel...\033[0m 🤞')
     try:
-        print('\033[94mTrying to send message to channel...\033[0m 🤞')
         await channel.send('I\'m alive! 😀')
         print(f'\033[92mMessage sent to {channel.name}!\033[0m 😀')
     except:
         print(f'\033[91mInvalid channel id!\033[0m 😭')
-
-@bot.command(name='test')
-async def test(ctx):
-    """Simple test command."""
-    await ctx.send('It worked! 👌')
+    bot.add_cog(Test(bot))
 
 bot.run(TOKEN)
